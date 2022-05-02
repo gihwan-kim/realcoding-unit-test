c02a39c4e5e5e1e78d8642de26c669a73d2aa5ef
Ryu-JM <jminryu@naver.com>
2022-05-02 17:36:50 +0900

    text modify

--- src/__tests__/CalculatorDivide.spec.js
+++ src/__tests__/CalculatorDivide.spec.js
@@ -1,11 +1,11 @@
 import Calculator from "../js/calculator/Calculator";
 
 describe("사직연산 나눗셈 테스트.", () => {
-  test("10에서 2를 나누면 5이다.", () => {
+  test("10를 2으로 나누면 5이다.", () => {
     const calculator = new Calculator();
     expect(calculator.divide(10, 2)).toEqual(5);
   });
-  test("10에서 0으로 나눌 수 없다.", () => {
+  test("0으로 나누면 에러가 발생한다.", () => {
     const calculator = new Calculator();
     expect(calculator.divide(10, 0)).toEqual("Divide by Zero");
   });
