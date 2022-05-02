8f6710c233944216700cc78cf69f15f60accd42d
giwhan-kim <kgh06079@naver.com>
2022-05-02 18:19:52 +0900

    divided by zerro

--- src/js/calculator/Calculator.js
+++ src/js/calculator/Calculator.js
@@ -1,3 +1,10 @@
+export class DivideZeroError extends Error {
+    constructor() {
+        super('0으로는 나눌 수 없지');
+        this.name = 'Divide Zero Error';
+    }
+}
+
 class Calculator {
     plus = (a, b) => {
         return a + b;
@@ -12,7 +19,11 @@ class Calculator {
     }
 
     divide = (a, b) => {
+        if (b === 0) {
+            throw new DivideZeroError
+        }
         return a / b;
     }
 }
+
 export default Calculator;
